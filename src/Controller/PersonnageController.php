aecf0799413da7e3a76daf262a11220ad7eeeebb
Rizvane <rizvane95140@gmail.com>
2020-02-16 15:48:17 +0100

    ajout personnages

--- src/Controller/PersonnageController.php
+++ src/Controller/PersonnageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PersonnageController extends AbstractController
 {
     /**
-     * @Route("/personnage", name="personnage")
+     * @Route("/", name="accueil")
      */
     public function index()
     {
@@ -16,4 +16,21 @@ class PersonnageController extends AbstractController
             'controller_name' => 'PersonnageController',
         ]);
     }
+
+    /**
+     * @Route("/personnages", name="personnages")
+     */
+    public function persos()
+    {
+        return $this->render('personnage/persos.html.twig', [
+            'tab_test' => [
+                'force' => 3,
+                'exp' => 2,
+                'mana' => 12
+            ],
+        ]);
+    }
+
+
+
 }
